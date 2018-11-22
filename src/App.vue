dd48f1c50ac232a01bc8fe4a8d9137d04ef7f983
AlexScherbakov <alsherbakov13@gmail.com>
2018-11-22 12:13:53 +0400

    Arrays in vue

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,10 @@
     <ul>
       <li v-for="(item, index) of items">{{parentText}} {{item}}-index={{index}}</li>
     </ul>
+    <button v-on:click="addItem">Добавить элемент в конец массива</button>
+    <p>   в этом случае мы добавляем элемнт в массив с помощью метода push, а он по умолчанию обёрнут в реактивность фреймворком, поэтому представление обновляется вместе с данными</p>
+   <button v-on:click="addItemBadPractice">Добавление в масси по индексу</button>
+   <p>В этом случае обновления представления не происходит т.к. добавление по индексу не реализован реактивно в фреймворке, но мы можнем удостовериться в том, что на самом деле добавление происходит нажав на первую кнопку. В общем это плохой способ и так делать не надо!</p>
     <p>так и свойства объектов</p>
     <ul>
       <li v-for="(prop, key, index) in user">{{index}}. {{key}}: {{prop}}</li>
@@ -23,6 +27,23 @@ export default {
         lastName: 'Shcherbackov',
         age: 27
       }
+    }
+  },
+  methods: {
+    addItem(){
+       
+      let lastNumberPart = this.items[this.items.length - 1]
+        .split('')
+        .filter((char)=>{
+          return parseInt(char);
+        })
+        .join('');
+
+      this.items.push(`text${++lastNumberPart}`);
+    
+    },
+    addItemBadPractice(){
+      this.items[this.items.length] = "badPractice";
     }
   }
 }
